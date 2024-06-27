3806dd5ccc4bfde79f727f7267e529cae6630959
thanhhai0311 <nguyenthanhhai03112003@gmail.com>
2024-06-27 10:01:23 +0700

    create-project

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// const workprocessSwiper = new Swiper('.workprocessSwiper', {
+//   // Optional parameters
+//   slidesPerView: 6,
+//   spaceBetween: 30,
+//   loop: true,
+
+// });
+
+
+const testimonialsSwiper = new Swiper('.testimonialsSwiper', {
+  // Optional parameters
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+
+});
+
+
